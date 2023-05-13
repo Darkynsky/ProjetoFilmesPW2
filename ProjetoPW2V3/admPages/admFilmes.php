04c7e6dfc6307d3afe96d6419b2c6d4deff6f0ca
Darkynsky <telles861@gmail.com>
2023-05-12 22:09:42 -0300

    formulario com função de editar que não funciona

--- ProjetoPW2V3/admPages/admFilmes.php
+++ ProjetoPW2V3/admPages/admFilmes.php
@@ -1,4 +1,4 @@
-<section id="filme">
+<section id="filme_inserir">
     <form action="filme-salvar.php" method="post">
         <div>
             <input type="text" placeholder="Filme" name="txFilme" class="campFormContato"/>
@@ -29,6 +29,58 @@
     </form>
 </section>
 
+<!-- FORMULARIO DE EDIÇÃO -->
+<section id="filme_editar">
+    <form action="filme-editar.php" method="post">
+        <div>
+            <input
+                type="hidden"
+                name="txIdFilme"
+                class="campFormContato"
+                value="<?php echo @$_GET['idfilme']; ?>"/>
+        </div>
+        <div>
+            <input
+                type="text"
+                placeholder="Filme"
+                name="txFilme"
+                class="campFormContato"
+                value="<?php echo @$_GET['filme']; ?>"/>
+        </div>
+
+        <div>
+            <input
+                type="text"
+                placeholder="Gênero"
+                name="txGenero"
+                class="campFormContato"
+                value="<?php echo @$_GET['genero']; ?>"/>
+        </div>
+
+        <div>
+            <input
+                type="text"
+                placeholder="Ano de lançamento"
+                name="txAno"
+                class="campFormContato"
+                value="<?php echo @$_GET['ano']; ?>"/>
+        </div>
+
+        <div>
+            <textarea
+                placeholder="Descrição do filme"
+                name="txDescricao"
+                class="campFormContatoTEXTAREA"><?php echo @$_GET['descricao']; ?></textarea>
+        </div>
+
+        <div>
+            <input type="submit" value="SALVAR EDIÇÃO" class="btn_salvar"/>
+        </div>
+    </form>
+</section>
+<!-- FIM DO FORMULÁRIO DE EDIÇÃO -->
+
+
 <table class="tbExibicao">
     <thead>
         <tr>
@@ -53,6 +105,7 @@
                     echo "<td> $row[3] </td>";											
                     echo "<td> $row[4] </td>";
                     echo "<td> 
+                            <a href='?id=$row[0]&filme=$row[1]&genero=$row[2]&ano=$row[3]&descricao=$row[4]'> Editar </a>
                             <a href='filme-remover.php?id=$row[0]'> Remover </a>
                           </td>";
                   echo "</tr>";
